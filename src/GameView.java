852c14013b5780235da2662a7418224358d31c9f
brandon <86nguyenb@gmail.com>
2016-10-18 13:06:13 -0400

    Setup a basic GUI

---
+++ src/GameView.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+
+/**
+ * GameView.java
+ *
+ * This class shows game changes and updates to the user.
+ * Implements Java GUI classes.
+ *
+ * @author Brandon Nguyen, nguye299@purdue.edu, Lab Section G06
+ *
+ * @version October 18, 2016
+ *
+ */
+public class GameView extends JFrame {
+
+    public final String TITLE = "Psych";
+    public final int FRAME_WIDTH = 300;
+    public final int FRAME_HEIGHT = 500;
+
+    public GameView() {
+        setTitle(TITLE);
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+
+}
+
+
+/**
+ * Purpose of Method
+ * @param parameters (may be more than one)
+ * @return (if not void)
+ * Methods*/
