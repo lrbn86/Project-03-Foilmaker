f3ab1dc488e8b003809e42330f669bf0c8646557
brandon <86nguyenb@gmail.com>
2016-10-18 13:09:34 -0400

    Setup required logistic-variables (login details, number of players, etc.)

---
+++ src/GameModel.java
@@ -0,0 +1,20 @@
+/**
+ * GameModel.java
+ *
+ * This class maintains data structures that reflects the internal state of the whole application.
+ * Captures data (questions, qord suggestions, etc. received from the server)
+ *
+ * @author Brandon Nguyen, nguye299@purdue.edu, Lab Section G06
+ *
+ * @version October 18, 2016
+ *
+ */
+public class GameModel {
+
+    // Logistics
+    private String username;
+    private String password;
+    private int numOfUsers;
+
+}
+
