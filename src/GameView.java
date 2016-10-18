6e0f21c4929cd6a6357fc7ea005123c9aee16b4c
brandon <86nguyenb@gmail.com>
2016-10-18 13:22:35 -0400

    Added buttons and text fields

--- src/GameView.java
+++ src/GameView.java
@@ -17,6 +17,21 @@ public class GameView extends JFrame {
     public final int FRAME_WIDTH = 300;
     public final int FRAME_HEIGHT = 500;
 
+    // Buttons
+    private JButton login = new JButton ("Login");
+    private JButton register = new JButton ("Register");
+    private JButton startNewGame = new JButton ("Start New Game");
+    private JButton joinGame = new JButton ("Join a Game");
+    private JButton submitSuggestion = new JButton ("Submit Suggestion");
+    private JButton submitOption = new JButton ("Submit Option");
+    private JButton nextRound = new JButton ("Next Round");
+
+    // Input Fields
+    private JTextField gameKeyInput = new JTextField();
+    private JTextField userSuggestionInput = new JTextField();
+
+
+
     public GameView() {
         setTitle(TITLE);
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
