1c67d981cc7889eaf3e33f552632088fb32c46a6
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-10-25 22:55:58 -0400

    Update GameModel.java

--- src/GameModel.java
+++ src/GameModel.java
@@ -4,7 +4,7 @@
  * This class maintains data structures that reflects the internal state of the whole application.
  * Captures data (questions, qord suggestions, etc. received from the server)
  *
- * @author Brandon Nguyen, nguye299@purdue.edu, Lab Section G06
+ * @author Brandon Nguyen & Daniel Acevedo, nguye299@purdue.edu & acevedd@purdue.edu, Lab Section G06
  *
  * @version October 18, 2016
  *
