d028867c3a39e557cade2d41648411b95f4b6fc0
brandon <86nguyenb@gmail.com>
2016-10-25 20:07:14 -0400

    Initial Commit

---
+++ src/FoilerMakerNetworkProtocol.java
@@ -0,0 +1,13 @@
+/**
+ * FoilerMakerNetworkProtocol.java
+ *
+ *
+ *
+ * @author Brandon Nguyen, nguye299@purdue.edu, Lab Section G06
+ *
+ * @version October 25, 2016
+ *
+ */
+public class FoilerMakerNetworkProtocol {
+
+}
