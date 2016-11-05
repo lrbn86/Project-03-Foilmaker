80d72073772affd23ca9e744680ab6bea1a9bb73
acevedd <acevedd@purdue.edu>
2016-11-05 15:27:48 -0400

    Simplified
    Added server connection code to constructor. Possible future edit can be to declare a random integer between 0 and 65536.

--- src/Foilermaker.java
+++ src/Foilermaker.java
@@ -1,3 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
 /**
  * Foilermaker.java
  *
@@ -13,37 +21,43 @@ public class Foilermaker {
 
     FMModel model;
     FMView view;
-
-    public Foilermaker () {
+    private Socket socket ;
+    private PrintWriter out ;
+    private BufferedReader in ;
+    
+    //Constructor takes care of server connection
+    public Foilermaker ()throws Exception {
         model = new FMModel(this);
         view = new FMView(this);
-    }
-
-    public void startGame() {
-        connectToServer();
-    }
-
-
-    // Networking
-    public void connectToServer() {
-
-        String serverHost = view.serverHostInput.getText();
-        int portNumber = Integer.parseInt(view.portNumberInput.getText());
-
+        
         try {
-            System.out.println("Connecting to Server...");
-            Socket socket = new Socket (serverHost, portNumber);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        	System.out.println("Connecting to Server...");
+    		socket = new Socket ("localhost", 9000);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         } catch (IOException e) {
         }
-
     }
+       
+    //Requests addition of a new user
+	public void createUser()throws Exception{
+		out.println("CREATENEWUSER" + "--" + view.usernameInput.getText() + "--" + view.passwordInput.getText());
+    	
+    }
+	
+	//Logs in existing user
+	public void loginUser()throws Exception{
+        out.println("LOGIN" + "--" + view.usernameInput.getText() + "--" + view.passwordInput.getText());
+	}
 
-    public static void main(String[] args) {
-        Foilermaker game = new Foilermaker();
-        game.startGame();
-
+    public static void main(String[] args)throws Exception {
+    	
+    	Foilermaker game = new Foilermaker();
+  
+        
+        
+        
     }
 }
