11fdd048b1b5bdf1226ac505afd332d9c63de49a
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-10 15:25:23 -0500

    Update Foilermaker.java
    Complete revision

--- src/Foilermaker.java
+++ src/Foilermaker.java
@@ -1,106 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-
 /**
- * Foilermaker.java
+ * Foilmaker.java
  *
- * This class manipulates data in the model as per user interactions.
- * Implements network programming pits, application protocol, etc.
+ * Game Main Entry Point
  *
  * @author Brandon Nguyen & Daniel Acevedo, nguye299@purdue.edu & acevedd@purdue.edu, Lab Section G06
  *
- * @version November 7, 2016
+ * @version November 10, 2016
  *
  */
-public class Foilermaker {
+public class Foilmaker {
 
-    FMModel model;
-    FMView view;
-    FoilMakerNetworkProtocol p;
+    private FMController controller;
+    private FMView view;
+    private FMModel model;
 
-    String serverResponse;
-
-    Socket socket;
-    PrintWriter out;
-    InputStreamReader isr;
-    BufferedReader in;
-
-    public Foilermaker () {
-        model = new FMModel(this);
-        view = new FMView(this);
-        p = new FoilMakerNetworkProtocol();
-    }
-
-    public void startGame() {
-        connectToServer();
-    }
-
-
-    // Networking
-    public void connectToServer() {
-
-        String serverHost = view.serverHostInput.getText();
-        int portNumber = Integer.parseInt(view.portNumberInput.getText());
-
-        try {
-            System.out.println("Connecting to Server...");
-
-            // Connect to server
-            socket = new Socket(serverHost, portNumber);
-
-//            // Create data writer
-//            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-//
-//            // Create data reader
-//            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
-//            BufferedReader in = new BufferedReader(isr);
-
-            // Send message to server
-//            out.println();
-
-            // Read server response
-//            String serverMessage = in.readLine();
-//            System.out.println(serverMessage);
-            System.out.println("Connected to Server.");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void sendNewUserRequest() {
-
-        model.setUsername(view.usernameInput.getText());
-        model.setPassword(view.passwordInput.getText());
-
-        try {
-            out = new PrintWriter(socket.getOutputStream(), true);
-            out.println(FoilMakerNetworkProtocol.MSG_TYPE.CREATENEWUSER + p.SEPARATOR + model.getUsername() + p.SEPARATOR + model.getPassword());
-            while (in.readLine() != null) {
-                setServerResponse(in.readLine());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public String getServerResponse() {
-        return this.serverResponse;
-    }
-
-    public void setServerResponse(String serverResponse) {
-        this.serverResponse = serverResponse;
+    public Foilmaker() {
+        view = new FMView();
+        model = new FMModel();
+        controller = new FMController(model, view);
     }
 
     public static void main(String[] args) {
-        Foilermaker game = new Foilermaker();
-        game.startGame();
-        System.out.println(game.getServerResponse());
-
+        Foilmaker game = new Foilmaker();
     }
+
 }
