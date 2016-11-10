95a6b8366b2706b13022896cc3347598cc1a459a
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-11-10 15:22:34 -0500

    Update FMModel.java

--- src/FMModel.java
+++ src/FMModel.java
@@ -6,7 +6,7 @@
  *
  * @author Brandon Nguyen & Daniel Acevedo, nguye299@purdue.edu & acevedd@purdue.edu, Lab Section G06
  *
- * @version November 7, 2016
+ * @version November 10, 2016
  *
  */
 public class FMModel {
