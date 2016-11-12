30b92be526db1e978244450597698fd0675e46a9
acevedd <acevedd@purdue.edu>
2016-11-11 20:17:11 -0500

    Update FMView.java

--- src/FMView.java
+++ src/FMView.java
@@ -1,118 +1,103 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
 
 /**
  * FMView.java
  *
- * This class shows game changes and updates to the user.
- * Implements Java GUI classes.
+ * GUI
  *
- * @author Brandon Nguyen & Daniel Acevedo, nguye299@purdue.edu & acevedd@purdue.edu, Lab Section G06
+ * Brandon Nguyen & Daniel Acevedo, nguye299@purdue.edu & acevedd@purdue.edu, Lab Section G06
  *
- * @version October 18, 2016
+ * @version November 11, 2016
  *
  */
-public class FMView extends JFrame implements ActionListener{
+public class FMView extends JFrame {
 
-    private Foilermaker controller;
-    private FMModel model;
-    public final static String GAME_TITLE = "FoilerMaker";
-    public final static int FRAME_WIDTH = 310;
-    public final static int FRAME_HEIGHT = 500;
+    private JButton loginButton = new JButton ("Login");
+    private JButton registerButton = new JButton ("Register");
+    private JButton startNewGameButton = new JButton ("Start New Game");
+    private JButton joinAGameButton = new JButton ("Join a Game");
+    private JButton startGameButton = new JButton ("Start Game");
+    private JButton joinGameButton = new JButton ("Join Game");
+    private JButton submitSuggestionButton = new JButton ("Submit Suggestion");
+    private JButton submitOptionButton = new JButton ("Submit Option");
+    private JButton nextRoundButton = new JButton ("Next Round");
 
-    private JLabel title = new JLabel("FoilerMaker!");
-    private JLabel statusMessage = new JLabel("Login or register");
-    private JLabel usernameLabel = new JLabel("Username: ");
-    private JLabel passwordLabel = new JLabel("Password: ");
-    private JLabel userInstruction = new JLabel("Other users should use this key to join your game: ");
-    private JLabel joinInstruction = new JLabel("Enter the game key to join a game");
-    private JLabel useKey = new JLabel("Something");
-    private JLabel waiting = new JLabel("Waiting for leader...");
+    private JTextField usernameInput = new JTextField (8);
+    private JPasswordField passwordInput = new JPasswordField (8);
+    private JTextField gameKeyOutput = new JTextField (3);
+    private JTextField gameKeyInput = new JTextField (3);
+    private JTextField suggestionInput = new JTextField(10);
+    private JTextArea participantsOutput = new JTextArea(3, 0);
 
-    JButton connectButton = new JButton ("Connect");
-    JButton loginButton = new JButton ("Login");
-    JButton registerButton = new JButton("Register");
-    JButton newGameButton = new JButton("Start a new game");
-    JButton joinGameButton = new JButton("Join a game");
-    JButton startGame = new JButton("Start game");
-    JButton joinGame = new JButton("Join game");
+    private JLabel title = new JLabel ("FoilMaker!");
+    private JLabel statusMessage = new JLabel ();
+    private JLabel word = new JLabel();
+    private JLabel descriptionOutput = new JLabel();
 
-    JTextField usernameInput = new JTextField(8);
-    JTextField keyInput = new JTextField(3);
-    JPasswordField passwordInput = new JPasswordField(8);
+    private JPanel top = new JPanel();
+    private JPanel states = new JPanel (new CardLayout());
+    private JPanel bottom = new JPanel();
+//    private JPanel CONNECT_STATE = new JPanel(new GridBagLayout());
+    private JPanel LOGIN_REGISTER_STATE = new JPanel (new GridBagLayout());
+    private JPanel NEW_OR_JOIN_GAME_STATE = new JPanel(new GridBagLayout());
+    private JPanel LEADER_VIEW_STATE = new JPanel(new GridBagLayout());
+    private JPanel PARTICIPANT_VIEW_STATE = new JPanel(new GridBagLayout());
+    private JPanel PARTICIPANT_WAIT_VIEW_STATE = new JPanel(new GridBagLayout());
+    private JPanel GIVE_SUGGESTION_STATE = new JPanel (new GridBagLayout());
+    private JPanel SUBMIT_OPTION_STATE = new JPanel (new GridBagLayout());
+    private JPanel RESULT_STATE = new JPanel (new GridBagLayout());
 
-    JPanel top = new JPanel();
-    JPanel middle = new JPanel(new CardLayout());
-    JPanel bottom = new JPanel();
-    
-
-    JPanel CONNECT_STATE = new JPanel(new GridBagLayout());
-    JPanel LOGIN_REGISTER_STATE = new JPanel (new GridBagLayout());
-    JPanel NEW_OR_JOIN_GAME_STATE = new JPanel(new GridBagLayout());
-    JPanel NEW_GAME_STATE = new JPanel(new GridBagLayout());
-    JPanel JOIN_GAME_STATE = new JPanel(new GridBagLayout());
-    JPanel activity = new JPanel();
-    JPanel wait = new JPanel(new GridBagLayout());
-
-    public FMView(Foilermaker controller, FMModel model) {
-        this.controller = controller;
-        this.model = model;
-        setTitle(GAME_TITLE);
-        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    public FMView() {
+        setTitle("FoilMaker");
+        setPreferredSize(new Dimension(300, 500));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setUpGUI();
-        //this.pack();		commented out as it changes the set frame width & height
+        setResizable(false);
+        pack();
+        setupGUI();
         setVisible(true);
     }
-    public void setTitleMessage(String msg){
-    	title.setText(msg);
-    }
-    public void setStatusMessage(String msg) {
-        statusMessage.setText(msg);
-    }
-    public void setUseKey(String key){
-    	useKey.setText(key);
-    }
-    public void setUpGUI() {
+
+    public void setupGUI() {
         top.add(title);
         add(top, BorderLayout.NORTH);
 
-        middle.setBorder(BorderFactory.createEtchedBorder());
-        add(middle, BorderLayout.CENTER);
+        states.setBorder(BorderFactory.createEtchedBorder());
+
+        states.add(getLoginRegisterState(), "LOGIN_REGISTER_STATE");
+        states.add(getNewOrJoinGameState(), "NEW_OR_JOIN_GAME_STATE");
+        states.add(getLeaderViewState(), "LEADER_VIEW_STATE");
+        states.add(getParticipantViewState(), "PARTICIPANT_VIEW_STATE");
+        states.add(getParticipantWaitViewState(), "PARTICIPANT_WAIT_VIEW_STATE");
+        states.add(getGiveSuggestionState(), "GIVE_SUGGESTION_STATE");
+        states.add(getSubmitOptionState(), "SUBMIT_OPTION_STATE");
+        states.add(getResultState(), "RESULT_STATE");
+        add(states, BorderLayout.CENTER);
 
         bottom.add(statusMessage);
         add(bottom, BorderLayout.SOUTH);
-       
-        addToMiddlePanel();
-        //do{
-        showLoginRegisterState();
-        //}while();
-        showNewOrJoinGameState();
-        showNewGameState();
-        showJoinGameState();
-        showWaitState();
-
     }
 
-    public void addToMiddlePanel() {
-        middle.add(LOGIN_REGISTER_STATE, "LOGIN_REGISTER_STATE");
-        middle.add(NEW_OR_JOIN_GAME_STATE, "NEW_OR_JOIN_GAME_STATE");
-        middle.add(NEW_GAME_STATE, "NEW_GAME_STATE");
-        middle.add(JOIN_GAME_STATE, "JOIN_GAME_STATE");
-        middle.add(wait, "wait");
+    public void setStatusMessage(String message) {
+        this.statusMessage.setText(message);
     }
 
-    public void showLoginRegisterState() {
+    public void setTitle(String title) {
+        this.title.setText(title);
+    }
+
+    public void setView(String view) {
+        CardLayout c = (CardLayout) states.getLayout();
+        c.show(states, view);
+    }
+
+    public JPanel getLoginRegisterState() {
         GridBagConstraints c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 0;
-
-        LOGIN_REGISTER_STATE.add(usernameLabel, c);
+        LOGIN_REGISTER_STATE.add(new JLabel("Username:"), c);
         c.gridy++;
-        LOGIN_REGISTER_STATE.add(passwordLabel, c);
+        LOGIN_REGISTER_STATE.add(new JLabel("Password:"), c);
         c.gridy++;
         LOGIN_REGISTER_STATE.add(loginButton, c);
         c.gridx = 1;
@@ -122,174 +107,146 @@ public class FMView extends JFrame implements ActionListener{
         LOGIN_REGISTER_STATE.add(passwordInput, c);
         c.gridy++;
         LOGIN_REGISTER_STATE.add(registerButton, c);
-        
-        loginButton.addActionListener(new ActionListener(){
-    		@Override
-    		public void actionPerformed(ActionEvent arg0){
-    			
-			try{
-				controller.loginUser();
-			}catch(IOException e){
-			}
-			if(controller.getExceptionThrown() == false){
-    			CardLayout c = (CardLayout) middle.getLayout();
-    			setStatusMessage("Welcome!");
-                c.show(middle, "NEW_OR_JOIN_GAME_STATE");
-                setTitleMessage(model.getUsername());
-			}else{
-				setStatusMessage(controller.getServerInput());
-			}
-    		}
-    	});
-        
-    	registerButton.addActionListener(new ActionListener(){
-    		@Override
-    		public void actionPerformed(ActionEvent arg0){
-    			try {
-					controller.createUser();
-				} catch (Exception e) {
-				}
-    			
-    			if(controller.getExceptionThrown() == false){
-        			setStatusMessage("New user created");
-    			}else{
-    				setStatusMessage(controller.getServerInput());
-    			}
-    		}
-    	});
-    	
-    }
-    
-    public void showNewOrJoinGameState(){
-    	GridBagConstraints c = new GridBagConstraints();
-    	c.gridx = 0;
-        c.gridy = 0;
-        
-        NEW_OR_JOIN_GAME_STATE.add(newGameButton, c);
-        c.gridx = 1;
-        c.gridy = 0;
-      
-        
-        NEW_OR_JOIN_GAME_STATE.add(joinGameButton, c);
-        
-        
-        newGameButton.addActionListener(new ActionListener(){
-        	@Override
-        	public void actionPerformed(ActionEvent arg0){
-        		try {
-					controller.startNewGame();
-				} catch (IOException e) {
-				}
-        		CardLayout c = (CardLayout) middle.getLayout();
-        		c.show(middle, "NEW_GAME_STATE");
-        		setStatusMessage("Game started: You are the leader");
-        		setUseKey(model.getGameKey());
-        		
-        		
-        	}
-        });
-        
-        joinGameButton.addActionListener(new ActionListener(){
-        	@Override
-        	public void actionPerformed(ActionEvent arg0){
-        		CardLayout c = (CardLayout) middle.getLayout();
-        		c.show(middle, "JOIN_GAME_STATE");
-        		setStatusMessage("Welcome!");
-        		
-        	}
-        });
-        
-    }
-    
-    //TODO: Fix host's panel to match that in handout.
-    public void showNewGameState(){
-    	GridBagConstraints c = new GridBagConstraints();
-    	
-    	c.gridx = 0;
-        c.gridy = 0;
-        
-        NEW_GAME_STATE.add(userInstruction, c);
-        c.gridy++;
-        
-        NEW_GAME_STATE.add(useKey, c);
-        c.gridy++;
-       
-        activity.setBorder(BorderFactory.createTitledBorder("Participants"));
-        activity.setPreferredSize(new Dimension(300, 200));
-        activity.setBackground(Color.LIGHT_GRAY);
-        NEW_GAME_STATE.add(activity);
-        c.gridx--;
-        c.gridy--;
-        
-        NEW_GAME_STATE.add(startGame,c);
-        c.gridy++;
-        
-        //disable or enables start game button
-        if(model.getNumOfParticipants() < 1){
-        	startGame.setEnabled(false);
-        }else{
-        	startGame.setEnabled(true);
-        }
-        
-        
-        startGame.addActionListener(new ActionListener(){
-        	@Override
-        	public void actionPerformed(ActionEvent arg0){
-        	
-        		startGame.setEnabled(true);
-        		try {
-					//create controller.startGame();
-				} catch (IOException e) {
-				}
-        		setStatusMessage("Game started: You are the leader");
-        		
-        	}
-        	
-        });
-        
-    }
-    
-    public void showJoinGameState(){
-    	GridBagConstraints c = new GridBagConstraints();
-    	
-    	c.gridx = 0;
-        c.gridy = 0;
-        
-        JOIN_GAME_STATE.add(joinInstruction, c);
-        c.gridy++;
-        
-        JOIN_GAME_STATE.add(keyInput, c);
-        c.gridy++;
-        
-        JOIN_GAME_STATE.add(joinGame, c);
-        c.gridx--;
-        c.gridy--;
-        
-        joinGame.addActionListener(new ActionListener(){
-        	@Override
-        	public void actionPerformed(ActionEvent arg0){
-        		try {
-					controller.joinGame();
-				} catch (IOException e) {
-				}
-        		CardLayout c = (CardLayout) middle.getLayout();
-        		c.show(middle, "wait");
-        		setStatusMessage("Joined game: waiting for leader");
-        	}
-        	
-        });
-    }
-    
-    public void showWaitState(){
-    	GridBagConstraints c = new GridBagConstraints();
-    	c.gridx = 0;
-        c.gridy = 0;
-        
-        wait.add(waiting, c);
-    	
-    }
-    
 
-    public void actionPerformed(ActionEvent e) {
-   
+        return LOGIN_REGISTER_STATE;
     }
+
+    public JPanel getNewOrJoinGameState() {
+        NEW_OR_JOIN_GAME_STATE.add(startNewGameButton);
+        NEW_OR_JOIN_GAME_STATE.add(joinAGameButton);
+
+        return NEW_OR_JOIN_GAME_STATE;
+    }
+
+    public JPanel getLeaderViewState() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        LEADER_VIEW_STATE.add(new JLabel("Others should use this key to join"), c);
+        c.gridy++;
+        LEADER_VIEW_STATE.add (gameKeyOutput, c);
+        c.gridy++;
+        participantsOutput.setPreferredSize(new Dimension (200, 250));
+        participantsOutput.setBorder(BorderFactory.createTitledBorder("Participants"));
+        LEADER_VIEW_STATE.add (participantsOutput, c);
+        c.gridy++;
+        startGameButton.setEnabled(false);
+        LEADER_VIEW_STATE.add (startGameButton, c);
+
+        return LEADER_VIEW_STATE;
+    }
+
+
+
+    public JPanel getParticipantViewState() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+
+        PARTICIPANT_VIEW_STATE.add(new JLabel("Enter the game key to join a game"), c);
+        c.gridy++;
+        PARTICIPANT_VIEW_STATE.add(gameKeyInput, c);
+        c.gridy++;
+        PARTICIPANT_VIEW_STATE.add(joinGameButton, c);
+
+        return PARTICIPANT_VIEW_STATE;
+    }
+
+    public JPanel getParticipantWaitViewState() {
+
+        PARTICIPANT_WAIT_VIEW_STATE.add(new JLabel("Waiting for leader ..."));
+
+        return PARTICIPANT_WAIT_VIEW_STATE;
+    }
+
+    // TODO
+    public JPanel getGiveSuggestionState() {
+    	 GridBagConstraints c = new GridBagConstraints();
+         c.gridx = 0;
+         c.gridy = 0;
+
+        GIVE_SUGGESTION_STATE.add(new JLabel("What is the word for"));
+        GIVE_SUGGESTION_STATE.add(descriptionOutput, c);
+        suggestionInput.setBorder(BorderFactory.createTitledBorder("Your suggestion"));
+        GIVE_SUGGESTION_STATE.add(suggestionInput);
+        GIVE_SUGGESTION_STATE.add(submitSuggestionButton, c);
+        c.gridx ++;
+        c.gridy = 2;
+
+        return GIVE_SUGGESTION_STATE;
+    }
+
+    public JPanel getSubmitOptionState() {
+    	
+        return SUBMIT_OPTION_STATE;
+    }
+
+    public JPanel getResultState() {
+        return RESULT_STATE;
+    }
+
+
+    public JButton getLoginButton() {
+        return this.loginButton;
+    }
+
+    public JButton getRegisterButton() {
+        return this.registerButton;
+    }
+
+    public JButton getStartNewGameButton() {
+        return this.startNewGameButton;
+    }
+
+    public JButton getJoinAGameButton() {
+        return this.joinAGameButton;
+    }
+
+    public JButton getStartGameButton() {
+        return this.startGameButton;
+    }
+
+    public JButton getJoinGameButton() {
+        return this.joinGameButton;
+    }
+
+    public JButton getSubmitSuggestionButton() {
+        return this.submitSuggestionButton;
+    }
+
+    public JButton getSubmitOptionButton() {
+        return this.submitOptionButton;
+    }
+
+    public JButton getNextRoundButton() {
+        return this.nextRoundButton;
+    }
+
+    public JTextField getUsernameInput() {
+        return this.usernameInput;
+    }
+
+    public JTextField getGameKeyOutput() {
+        return this.gameKeyOutput;
+    }
+
+    public JTextField getGameKeyInput() {
+        return this.gameKeyInput;
+    }
+
+    public JTextArea getParticipantsOutput() {
+        return this.participantsOutput;
+    }
+
+    public JLabel getDescriptionOutput() {
+        return this.descriptionOutput;
+    }
+
+    public JPasswordField getPasswordInput() {
+        return this.passwordInput;
+    }
+    
+    public JTextField getSuggestionInput(){
+    	return this.suggestionInput;
+    }
+}
